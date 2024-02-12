bee9966c2b381a21a2e8946670ebfbc9481811ba
yelveshubham <shubhyelve45@gmail.com>
2024-02-12 01:28:06 -0500

    Initial commit

---
+++ student_management_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Base(request):
+    return render(request,'base.html')
+
+def Login(request):
+    return render(request,'login.html')
+
